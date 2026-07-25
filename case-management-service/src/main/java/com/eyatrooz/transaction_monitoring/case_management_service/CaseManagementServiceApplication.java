424aae2948f0934fa3dfb0067a143ad4ba9f5762
Emran Youssef Al-Khaleel <emrankhaleel03@gmail.com>
2026-07-26 01:30:36 +0300

    Confiqured all needed dependencies in pom file for case-managment-service

---
+++ case-management-service/src/main/java/com/eyatrooz/transaction_monitoring/case_management_service/CaseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eyatrooz.transaction_monitoring.case_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseManagementServiceApplication.class, args);
+	}
+
+}
