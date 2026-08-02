36ef57c22ab67da6a30ce07c40be784b4373feb8
Emran Youssef Al-Khaleel <emrankhaleel03@gmail.com>
2026-08-02 04:13:56 +0300

    feat(rule-engine-service): add scheduled Outbox poller

--- rule-engine-service/src/main/java/com/eyatrooz/transaction_monitoring/rule_engine_service/RuleEngineServiceApplication.java
+++ rule-engine-service/src/main/java/com/eyatrooz/transaction_monitoring/rule_engine_service/RuleEngineServiceApplication.java
@@ -3,9 +3,11 @@ package com.eyatrooz.transaction_monitoring.rule_engine_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
+@EnableScheduling
 public class RuleEngineServiceApplication {
 
 	public static void main(String[] args) {
