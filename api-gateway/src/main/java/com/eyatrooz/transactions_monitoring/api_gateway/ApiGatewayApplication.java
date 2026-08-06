36b8210256155739aea2b617904e7bbd21a455de
Emran Youssef Al-Khaleel <emrankhaleel03@gmail.com>
2026-08-07 00:26:19 +0300

    constructed Api-Gateway infastructure in pom file

---
+++ api-gateway/src/main/java/com/eyatrooz/transactions_monitoring/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.eyatrooz.transactions_monitoring.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/com/eyatrooz/transactions_monitoring/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyatrooz.transactions_monitoring.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
